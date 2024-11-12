06cc65270129123b93e633642e30a671b0cb4bfa
Tharlis Sampaio <tharlis.sampaio@outlook.com>
2024-11-11 23:01:26 -0500

    Initial commit

---
+++ src/main/java/br/com/desafio_todolist/TodolistBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio_todolist/TodolistBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
